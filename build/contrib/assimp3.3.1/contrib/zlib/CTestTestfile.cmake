515d52bedeced809bc3cda8450985e48583f02d6
Rafael Antunes <rafael.uminho@hotmail.com>
2017-03-29 02:50:22 +0100

    Update

---
+++ build/contrib/assimp3.3.1/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Rafael/Documents/GitHub/L-Systems_VSL/contrib/assimp3.3.1/contrib/zlib
+# Build directory: C:/Users/Rafael/Documents/GitHub/L-Systems_VSL/build/contrib/assimp3.3.1/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
